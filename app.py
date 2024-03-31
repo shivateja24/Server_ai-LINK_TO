8f66758e292ccc09064f088ce288b70b6560d345
Shivateja Gondala <94845621+shivateja24@users.noreply.github.com>
2024-03-31 18:17:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -555,5 +555,20 @@ worker_thread = Thread(target=worker)
 worker_thread.daemon = True
 worker_thread.start()
 print("worker started")
+
+
+@app.route('/')
+def greet():
+    # Get today's date and current time
+    now = datetime.now()
+    current_date_and_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    
+    # Customize the message with the name and current date and time
+    message = f"Hi Shiva Teja {current_date_and_time}"
+    
+    return message
+
+if __name__ == '__main__':
+    app.run(debug=True)
    
 
